9bf64957a7f42d30279f66d5bbc9c2c088bd782f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2016-06-04 06:35:45 -0400

    facets

--- facets2polymer.js
+++ facets2polymer.js
@@ -1,7 +1,10 @@
+var esprima = require('esprima');
 const filePath = './Tests/FlagIcon';
 //import flagIcon = require(filePath);
-//import flagIcon = require('./Tests/FlagIcon');
-var flagIcon = require(filePath);
+var flagIcon = require('./Tests/FlagIcon');
+//var flagIcon = require(filePath);
 const templateFnString = flagIcon.FlagIconTemplate.toString();
 console.log(templateFnString);
+const parsed = esprima.parse(templateFnString);
+console.log(esprima.parse);
 //# sourceMappingURL=facets2polymer.js.map
--- facets2polymer.ts
+++ facets2polymer.ts
@@ -1,10 +1,14 @@
 declare var require;
+var esprima = require('esprima');
 const filePath = './Tests/FlagIcon';
 
 
 //import flagIcon = require(filePath);
 import flagIcon = require('./Tests/FlagIcon');
+
 //var flagIcon = require(filePath);
 const templateFnString = flagIcon.FlagIconTemplate.toString();
 console.log(templateFnString);
+const parsed = esprima.parse(templateFnString);
+console.log(esprima.parse);
 
